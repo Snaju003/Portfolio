9619ddd2aea9cd2db89713e9d660712b0915f3aa
Snaju003 <soumyaraj2003@gmail.com>
2025-08-11 02:00:43 +0530

    added vercel analytics.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from '@vercel/analytics/next';
 
 // We'll use a direct link tag in the HTML head instead
 
@@ -62,6 +63,7 @@ export default function RootLayout({
 					{children}
 					<Toaster />
 				</ThemeProvider>
+				<Analytics />
 			</body>
 		</html>
 	);
